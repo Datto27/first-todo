3ed1278d91688500511b229c36fe2eb290fc2912
Dato27g <dato.getsadze.1@btu.edu.ge>
2021-08-30 17:36:47 +0400

    edit functional finish

--- src/TodoList.js
+++ src/TodoList.js
@@ -6,12 +6,14 @@ export const TodoList = ({data, deleteItem}) => {
     const {editItem, isEditing, setIsEditing,
             isActive, setIsActive} = useGlobalContext()
     const [curentItemID, setCurrentItemID] = useState("")
+    const [selectItemID, setSelectItemID] = useState("")
     const [newText, setNewText] = useState("")
-    const elementHeightRef = useRef(null)
+    const textHeightRef = useRef(null)
     // se function achvenebs mxolod axali inputis form-s da moaqvs curent itemis value(text)
     const showInput = (id) => {
         setCurrentItemID(id)
         setIsEditing(!isEditing)
+        // send a new text to context
         const specificItem = data.find(item => item.id === id)
         setNewText(specificItem.text)
     }
@@ -23,11 +25,11 @@ export const TodoList = ({data, deleteItem}) => {
     return <section className="todo-list">
         {data.map((item,) => {
             const {id, title, text} = item
-            return <div key={id} className="todo-item" 
-                        ref={elementHeightRef}>
+            return <div key={id} className="todo-item">
                     <header className="item-header">
                         <h1 className="title"
-                            onClick={() => showInput(id)}>{title}</h1>
+                            onClick={() => setSelectItemID(selectItemID ? null:id)}>
+                                {title}</h1>
                         <div className="btns">
                             <button className="edit-btn" 
                                 onClick={() => showInput(id)} >
@@ -39,16 +41,20 @@ export const TodoList = ({data, deleteItem}) => {
                             </button>
                         </div>
                     </header>
-                    {isEditing && id===curentItemID ? 
-                                (<form onSubmit={() => editItem(id, newText)}>
-                                    <textarea className="new-input" value={newText} 
-                                        onChange={(e) => setNewText(e.target.value)}></textarea>
-                                    <button className="save-btn" type="submit">
-                                        Save
-                                    </button>
-                                </form>)
-                                : (<p className="text">{text}</p>)
-                    }
+                    {<div className={`item-content ${selectItemID===id && "show-text"}`} 
+                        ref={textHeightRef}>
+                        {isEditing && id===curentItemID ? 
+                                    (<form onSubmit={() => editItem(id, newText)}>
+                                        <textarea className="new-input" value={newText} 
+                                            onChange={(e) => setNewText(e.target.value)}></textarea>
+                                        <button className="save-btn" type="submit">
+                                            Save
+                                        </button>
+                                    </form>)
+                                    : (<p className="text">{text}</p>)
+                        }
+                    </div>}
+                    {console.log(selectItemID, id)}
                     
                     {/* pirvel rigshi amotsmebs aris tu ara todo item actiuri, id-is sashualebit ( achvenebs <p> an "" )
                         shemdeg amotsmebs editi chartulia tu ara shesabamisi itemistvis
